8e62f30487b281e25180a443ff1fc51cb32e49c7
A01630826 <gravity.cp@hotmail.com>
2019-09-24 04:55:08 +0000

    practica 2 kill init

--- xv6-public/syscall.c
+++ xv6-public/syscall.c
@@ -140,7 +140,7 @@ syscall(void)
 
   num = curproc->tf->eax;
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
-    curproc->tf->eax = syscalls[num]();
+    curproc->tf->eax = syscalls[num](); //el parentesis hace que se ejecute
   } else {
     cprintf("%d %s: unknown sys call %d\n",
             curproc->pid, curproc->name, num);
